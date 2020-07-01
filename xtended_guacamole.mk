268f573f5fa82d68ed8a75063bb489ba76abc83b
Chandra <chandra.chaganti@hotmail.com>
2020-07-01 15:07:12 +0530

    guacamole: Initial commit

---
+++ xtended_guacamole.mk
@@ -0,0 +1,44 @@
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from guacamole device
+$(call inherit-product, device/oneplus/guacamole/device.mk)
+
+# Inherit some common Xtended stuff.
+$(call inherit-product, vendor/xtended/config/common_full_phone.mk)
+
+# Boot Animation
+TARGET_BOOT_ANIMATION_RES := 1440
+
+PRODUCT_NAME := xtended_guacamole
+PRODUCT_DEVICE := guacamole
+PRODUCT_MANUFACTURER := OnePlus
+PRODUCT_BRAND := OnePlus
+PRODUCT_MODEL := OnePlus 7 Pro
+
+PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+
+# Build info
+BUILD_FINGERPRINT := "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE=OnePlus7Pro \
+    PRODUCT_NAME=OnePlus7Pro \
+    PRIVATE_BUILD_DESC="OnePlus7Pro-user 10 QKQ1.190716.003 1909110008 release-keys"
